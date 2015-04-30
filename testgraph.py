151cbf1921413d19ac7cca5b85d8f9fddf711b4e
upschemistry <upschemistry@gmail.com>
2015-04-30 16:03:53 -0700

    Initial msg

---
+++ testgraph.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[10]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib')
+
+
+# In[14]:
+
+plt.figure()
+x = np.linspace(0,10);
+plt.plot(x,x**2)
+
